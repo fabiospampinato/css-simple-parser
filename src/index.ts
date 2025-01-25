2e703b15229e03785a898b97f984813d05b7d69a
Fabio Spampinato <spampinabio@gmail.com>
2025-01-25 20:27:34 +0000

    Ensuring some useful types are exported

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 import parse from './parse';
 import stringify from './stringify';
 import traverse from './traverse';
+import type {AST, ROOT_NODE, NODE} from './types';
 
 /* MAIN */
 
@@ -12,3 +13,4 @@ const Parser = { parse, stringify, traverse };
 /* EXPORT */
 
 export default Parser;
+export type {AST, ROOT_NODE, NODE};
